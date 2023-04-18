553cb67b20414f4dd2e9fe270e6651b3c8466f94
Aleksei_K <kytykob@gmail.com>
2023-04-18 23:13:03 +0300

    add readme, changed: script.js, style.css, index.html

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-// 12.35
+function generate(){
+    let quotes = {
+        "- Майкл Фелпс" : '"Цели никогда не должны быть простыми. Они должны быть неудобными, чтобы заставить вас работать"',
+        "- Леброн Джеймс" : '"Не бойтесь неудач, потому что это ваш путь к успеху"',
+        "- Арнольд Шварценеггер" : '"Причина создаёт ограничения. Но вы можете сделать многое, если верите в это всем своим сердцем"',
+        "- Генри Форд" : '"Неудача — это просто возможность начать снова, но уже более мудро"',
+    }
+
+    let authors = Object.keys(quotes);
+
+    let author = authors[Math.floor(Math.random() * authors.length)];
+    let quote = quotes[author];
+
+    document.getElementById('quote').innerHTML = quote;
+    document.getElementById('author').innerHTML = author;
+
+}
+
